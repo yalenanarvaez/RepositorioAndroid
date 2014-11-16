81c8e79e1274170461ca553a2e92f3790d51ea2d
yalenanarvaez <yalenanarvaez@gmail.com>
2014-11-16 18:47:53 -0500

    mapas otro layout

--- gen/com/example/reminderme/R.java
+++ gen/com/example/reminderme/R.java
@@ -2154,7 +2154,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030015;
         public static final int abc_simple_dropdown_hint=0x7f030016;
         public static final int activity_main=0x7f030017;
-        public static final int support_simple_spinner_dropdown_item=0x7f030018;
+        public static final int mapas=0x7f030018;
+        public static final int support_simple_spinner_dropdown_item=0x7f030019;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
