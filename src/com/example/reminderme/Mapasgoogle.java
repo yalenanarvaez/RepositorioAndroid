8f38716050770a4ccb5721a10b475abb4410adf3
yalenanarvaez <yalenanarvaez@gmail.com>
2014-11-16 19:47:37 -0500

    mapafin

---
+++ src/com/example/reminderme/Mapasgoogle.java
@@ -0,0 +1,54 @@
+package com.example.reminderme;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class Mapasgoogle extends ActionBarActivity {
+	 private GoogleMap googleMap;
+	  
+	   protected void onCreate(Bundle savedInstanceState) {
+	    
+	    	 super.onCreate(savedInstanceState);
+	         setContentView(R.layout.mapas);
+        
+	        try {
+	            // Loading map
+	            initilizeMap();
+	 
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+//
+
+
+	private void initilizeMap() {
+		// TODO Auto-generated method stub
+   	if (googleMap == null) {
+		Fragment fragment=getSupportFragmentManager().findFragmentById(R.id.map);
+		SupportMapFragment mapFragment= (SupportMapFragment) fragment;
+		googleMap = mapFragment.getMap();
+        //googleMap =  get).findFragmentById(R.id.map)).get
+       // check if map is created successfully or not
+        if (googleMap == null) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                    .show();
+            
+            
+       }
+    }	
+	}
+}
+
+
+
+
+
+
+
